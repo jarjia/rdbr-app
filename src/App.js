fbebfd20d195a9e1b348d511b1eb84b7c18c1cce
jarji <100996174+jarjia@users.noreply.github.com>
2022-09-04 15:36:58 +0400

    New Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<StartPage />}/>
         <Route path='/form' element={<Form />}/>
